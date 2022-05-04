6bb9e176f05135017ec0d242c7fde3275ec31a1b
TheRainMan <manar-yacine@outlook.com>
2022-05-03 17:49:48 -0700

    New db entries

--- myTODO/myTODOapp/urls.py
+++ myTODO/myTODOapp/urls.py
@@ -4,6 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),  
-    path("about/", views.about, name="about"),
+        path("<int:id>", views.index, name="index"),
 ]
